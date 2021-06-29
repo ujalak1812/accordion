0d1f1259be7283949cceb216bcf1b353c544b4f1
ujalakhurram <ujala.khurram@venturedive.com>
2021-06-29 13:42:01 +0500

    accordion v1

---
+++ README.md
@@ -0,0 +1,22 @@
+#### Idea
+
+[uidesigndaily](https://uidesigndaily.com/posts/sketch-accordion-website-day-1175)
+
+![](./idea.png);
+
+#### React Icons
+
+[react icons](https://react-icons.github.io/react-icons/)
+
+```
+npm install react-icons --save
+```
+
+```javascript
+import { FaHome } from 'react-icons/fa';
+const Component = () => {
+  return <FaHome className='icon'></FaHome>;
+};
+```
+## Demo
+[http://ujalak1812.github.io/accordion](http://ujalak1812.github.io/accordion)
---
+++ build/precache-manifest.f6b331b15c994208101197b0d356178d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88be94ebbb34d8266e0b840d9a317b3",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "f3f9073748c82c1833e2",
+    "url": "/accordion/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "899eee7dfc992c619951",
+    "url": "/accordion/static/js/2.fbe228ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion/static/js/2.fbe228ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f9073748c82c1833e2",
+    "url": "/accordion/static/js/main.03de2050.chunk.js"
+  },
+  {
+    "revision": "0af22f8fa2608fd03e14",
+    "url": "/accordion/static/js/runtime-main.fdc8c225.js"
+  }
+]);
